5f981d6d1b99be56805fc91c1d2dea2f529785ff
Filsan Musa <75952698+filsan95@users.noreply.github.com>
2024-05-08 03:00:09 -0400

    Create enable_cookies.sh

---
+++ enable_cookies.sh
@@ -0,0 +1,7 @@
+# This creates an alias for the cookies enabled Lynx browser.
+# Add this to your bash, zsh or other shell profile.
+# Add by running nano ~/.bash_profile or nano ~/.zshrc (you can vim or emacs in place of nano if preffered.)
+alias lynx='lynx -accept_all_cookies'
+# If using nano press CTRL + X to exit, and then enter 'Y' to save
+# vim: If using vim press 'i' first to insert the above line, and then 'esc' to exit writing mode. Then to use ':w' - to save, then ':q' - to exit. 
+# emacs: If using emacs press CTRL + X & CTRL + C - to initiate save, then enter - to confirm save. To exits CTRL + X & CTRL + C - to exit.
